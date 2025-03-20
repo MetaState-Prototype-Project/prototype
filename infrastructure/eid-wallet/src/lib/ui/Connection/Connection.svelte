7b2814305199e9e3b319bb9a9fb8453055e373d9
Ananya Rana <ananyarana2920@gmail.com>
2025-03-21 01:10:28 +0530

    created connection button

---
+++ infrastructure/eid-wallet/src/lib/ui/Connection/Connection.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+    import type { HTMLButtonAttributes } from 'svelte/elements';
+
+    interface IConnection extends HTMLButtonAttributes {
+        imgSrc: string;
+        connectionName: string;
+        lastConnected: Date;
+    }
+
+    let { imgSrc, connectionName, lastConnected, ...restProps}: IConnection = $props();
+
+</script>
+
+<div class={cn("flex items-center justify-between px-3 py-2", restProps.class)}>
+    <div class="flex items-center gap-2">
+        <div class="p-3 bg-gray-900 max-w-max rounded-4xl object-cover">
+            <img class="size-6" src={imgSrc} alt={connectionName}>
+        </div>
+        <div>
+            <h2 class="text-md font-medium text-black-900">{connectionName}</h2>
+            <p class="text-xs text-black-700">Last Active {lastConnected}</p>
+        </div>
+    </div>
+    <button 
+        class="p-2 text-md font-semibold text-primary-900"
+        {...restProps}
+    >
+            Disconnect
+    </button>
+</div>
--- infrastructure/eid-wallet/src/routes/+page.svelte
+++ infrastructure/eid-wallet/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import Connection from "$lib/ui/Connection/Connection.svelte";
+
 
 </script>
 
-<button class="btn btn-soft">Default</button>
+<!-- <button class="btn btn-soft">Default</button>
 <button class="btn btn-soft btn-primary">Primary</button>
 <button class="btn btn-soft btn-secondary">Secondary</button>
 <button class="btn btn-soft btn-accent">Accent</button>
@@ -13,4 +15,11 @@
 
 <h1 class="text-lg text-primary-900">Archivo font Everyone</h1>
 
-<div class="bg-secondary-700 text-primary-500 p-4">Secondary Background</div>
+<div class="bg-secondary-700 text-primary-500 p-4">Secondary Background</div> -->
+
+<Connection 
+    imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGPvo_J4nWlDM0kxFW0rsfR5UeOOC6uMvpfQ&s"
+    connectionName="Facebook.com"
+    lastConnected={new Date().toDateString() + ', ' + new Date().toLocaleTimeString()}
+    onclick={() => alert("Disconnected!")} 
+/>
