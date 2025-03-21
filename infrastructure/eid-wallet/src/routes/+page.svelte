a743077b461667ca5894554738fa3933458fd86d
JulienAuvo <julien@auvo.io>
2025-03-21 15:05:47 +0300

    chore: cleanup

--- infrastructure/eid-wallet/src/routes/+page.svelte
+++ infrastructure/eid-wallet/src/routes/+page.svelte
@@ -1,53 +1,15 @@
 <script lang="ts">
-  import SquircleDigit from '$lib/icons/SquircleDigit.svelte'
-  import * as Button from '$lib/ui/Button'
-
-  // dummy async function
-  async function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
-  }
-
-  async function dummyFunction() {
-    await delay(1000)
-    console.log('Button clicked!')
-  }
 </script>
+
+<button class="btn btn-soft">Default</button>
+<button class="btn btn-soft btn-primary">Primary</button>
+<button class="btn btn-soft btn-secondary">Secondary</button>
+<button class="btn btn-soft btn-accent">Accent</button>
+<button class="btn btn-soft btn-info">Info</button>
+<button class="btn btn-soft btn-success">Success</button>
+<button class="btn btn-soft btn-warning">Warning</button>
+<button class="btn btn-soft btn-error">Error</button>
 
 <h1 class="text-lg text-primary-900">Archivo font Everyone</h1>
 
 <div class="bg-secondary-700 text-primary-500 p-4">Secondary Background</div>
-<div class="flex gap-2 mx-4">
-  <div class="flex flex-col gap-2">
-    <Button.Action
-      cb={dummyFunction}
-      blockingClick
-      variant="solid"
-      class="w-full"
-    >
-      <SquircleDigit class="mr-2" />
-      Button
-    </Button.Action>
-    <Button.Action variant="solid" disabled
-      ><SquircleDigit class="mr-2" />Button</Button.Action
-    >
-    <Button.Action variant="danger" role="danger"
-      ><SquircleDigit class="mr-2" />Button</Button.Action
-    >
-  </div>
-  <div class="flex flex-col gap-2">
-    <Button.Action
-      cb={async () => {
-        await delay(1000)
-        console.log('Button clicked!')
-      }}
-      blockingClick
-      variant="soft"><SquircleDigit class="mr-2" />Button</Button.Action
-    >
-    <Button.Action variant="soft" disabled
-      ><SquircleDigit class="mr-2" />Button</Button.Action
-    >
-    <Button.Action variant="danger-soft"
-      ><SquircleDigit class="mr-2" />Button</Button.Action
-    >
-  </div>
-</div>
