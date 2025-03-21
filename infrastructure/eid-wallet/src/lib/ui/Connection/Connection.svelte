6eddfca704cd70bd23e20fa7557fc8c653370895
Ananya Rana <ananyarana2920@gmail.com>
2025-03-21 12:56:56 +0530

    added restprops to parent class

--- infrastructure/eid-wallet/src/lib/ui/Connection/Connection.svelte
+++ infrastructure/eid-wallet/src/lib/ui/Connection/Connection.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { cn } from '$lib/utils';
-    import type { HTMLButtonAttributes } from 'svelte/elements';
+    import type { HTMLAttributes } from 'svelte/elements';
 
-    interface IConnection extends HTMLButtonAttributes {
+    interface IConnection extends HTMLAttributes<HTMLElement>{
         imgSrc: string;
         connectionName: string;
         lastConnected: Date;
@@ -12,7 +12,7 @@
 
 </script>
 
-<div class={cn("flex items-center justify-between px-3 py-2", restProps.class)}>
+<div  {...restProps} class={cn("flex items-center justify-between px-3 py-2", restProps.class)}>
     <div class="flex items-center gap-2">
         <div class="p-3 bg-gray-900 max-w-max rounded-4xl object-cover">
             <img class="size-6" src={imgSrc} alt={connectionName}>
@@ -24,7 +24,6 @@
     </div>
     <button 
         class="p-2 text-md font-semibold text-primary-900"
-        {...restProps}
     >
             Disconnect
     </button>
