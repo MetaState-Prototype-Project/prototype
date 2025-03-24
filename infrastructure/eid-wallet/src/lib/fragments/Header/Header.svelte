02f1ae25b982fad39180c9b8b370b37a6d29a3b3
gourav <gourav@auvo.io>
2025-03-24 12:01:24 +0530

    chore: handlers added

--- infrastructure/eid-wallet/src/lib/fragments/Header/Header.svelte
+++ infrastructure/eid-wallet/src/lib/fragments/Header/Header.svelte
@@ -8,15 +8,18 @@
         title: string
         isUserLoggedIn?: boolean;
         isBackRequired?: boolean;
+        handleProfile?: () => void;
     }
 
-    let {title= "Create PIN", isUserLoggedIn = true, isBackRequired = true, ...restProps}:IHeaderProps = $props()
+    let {title= "Create PIN", isUserLoggedIn = true, isBackRequired = true,handleProfile=undefined, ...restProps}:IHeaderProps = $props()
     const cBase = "w-full h-[9vh] flex justify-between items-center"
 </script>
 
 <header {...restProps} class={cn(cBase, restProps.class)}>
     {#if isBackRequired}
-    <span class="flex justify-start">
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <span class="flex justify-start" onclick={() => window.history.back()}>
         <HugeiconsIcon size="5.5vw"  color="var(--color-black-700)" icon={ArrowLeft01Icon} />
     </span>
     {:else}
@@ -25,7 +28,7 @@
     {/if}
     <h1 class="text-3xl text-black text-center font-semibold">{title}</h1>
     {#if isUserLoggedIn}
-    <span class="flex justify-end">
+    <span class="flex justify-end" onclick={handleProfile}>
         <HugeiconsIcon size="8.1vw" color="var(--color-black-700)" icon={UserCircleIcon} />
     </span>
     {:else}
