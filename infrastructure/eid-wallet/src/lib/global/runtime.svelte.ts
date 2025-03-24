957b9c9efd1f93ac61c3098ec91d7377b4defe7e
gourav <gourav@auvo.io>
2025-03-24 12:42:49 +0530

    fix: added heading

--- infrastructure/eid-wallet/src/lib/global/runtime.svelte.ts
+++ infrastructure/eid-wallet/src/lib/global/runtime.svelte.ts
@@ -1,9 +1,11 @@
 export const runtime = $state<{
     header: {
+        title: string | undefined;
         backEnabled: boolean | undefined;
     }
 }>({
     header: {
+        title: undefined,
         backEnabled: undefined
     }
 })
