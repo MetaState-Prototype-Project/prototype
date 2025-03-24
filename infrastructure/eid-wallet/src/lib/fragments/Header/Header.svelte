b2d3ec05042d2a62c727e56be4a12edf0afa78ac
gourav <gourav@auvo.io>
2025-03-24 10:47:43 +0530

    feat: header props added

--- infrastructure/eid-wallet/src/lib/fragments/Header/Header.svelte
+++ infrastructure/eid-wallet/src/lib/fragments/Header/Header.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
+    import { Back } from "$lib/icons";
+    import Avatar from "$lib/icons/Avatar.svelte";
     import type { HTMLAttributes } from "svelte/elements";
 
     interface IHeaderProps extends HTMLAttributes<HTMLElement> {
+        title: string
         isUserLoggedIn?: boolean;
         isBackRequired?: boolean;
     }
+
+    let {title= "Create Pin", isUserLoggedIn = true, isBackRequired = true}:IHeaderProps = $props()
 </script>
 
-<header>
-    
+<header class="flex justify-between items-center">
+    <Back/>
+    <h1 class="">{title}</h1>
+    <Avatar/>
 </header>
