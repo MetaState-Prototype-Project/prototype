8492ae1c82cdb6faaf5b4ed49dac30456bb39c12
JulienAuvo <julien@auvo.io>
2025-03-25 10:15:22 +0300

    feat: test page for language selectors

--- infrastructure/eid-wallet/src/routes/julientest/+page.svelte
+++ infrastructure/eid-wallet/src/routes/julientest/+page.svelte
@@ -1,48 +1,33 @@
 <script lang="ts">
   import Selector from '$lib/ui/Selector/Selector.svelte'
+  import { getLanguageWithCountry } from '$lib/utils/getLanguage'
   import { writable } from 'svelte/store'
 
-  let selectors = [
-    {
-      id: 'gb',
-      value: 'English UK (Default)',
-      checked: true,
-    },
-    {
-      id: 'es',
-      value: 'Spanish',
-      checked: false,
-    },
-    {
-      id: 'de',
-      value: 'German',
-      checked: false,
-    },
-    {
-      id: 'fr',
-      value: 'French',
-      checked: false,
-    },
-    {
-      id: 'ru',
-      value: 'Russian',
-      checked: false,
-    },
-  ]
+  const AVAILABLE_LANGUAGES = ['en-GB', 'es', 'de', 'fr', 'ru']
+
+  const selectors = AVAILABLE_LANGUAGES.map((locale) => {
+    const { code, name } = getLanguageWithCountry(locale)
+
+    return {
+      id: code,
+      value: name,
+      checked: locale === 'en-GB',
+    }
+  })
 
   let selected = writable(selectors[0].value)
 </script>
 
 <h1 class="text-2xl font-bold">Select your language</h1>
 
-<fieldset class="mx-8 flex flex-col gap-12 mt-12">
+<fieldset class="mx-8 flex flex-col gap-4 mt-12">
   {#each selectors as selector}
     {@const { id, value, checked } = selector}
 
-    <Selector {id} name="lang" {value} {checked} bind:selected={$selected}>
-      {#snippet flagIcon(id: string)}
+    <Selector {id} name="lang" {value} bind:selected={$selected}>
+      {#snippet icon(id: string)}
         <div
-          class="rounded-full fi fis fi-{id} scale-150 mr-8 outline-8 outline-gray-900"
+          class="rounded-full fi fis fi-{id} scale-150 mr-12 outline-8 outline-gray-900"
         ></div>
       {/snippet}
       {value}
