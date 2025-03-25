5e3e1ef5f8e010bb6c63dcc5611cea13a5308566
gourav <gourav@auvo.io>
2025-03-25 14:39:48 +0530

    feat: onboarding-page

--- infrastructure/eid-wallet/src/routes/+page.svelte
+++ infrastructure/eid-wallet/src/routes/+page.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
+    import ButtonAction from "$lib/ui/Button/ButtonAction.svelte";
 
-    import { Drawer } from "$lib/ui";
-    let isPaneOpen = $state(false);
+
 </script>
 
-<button class="btn btn-soft" onclick={() => isPaneOpen = true}>Open</button>
-
-
-
-
-<Drawer bind:isPaneOpen isCancelRequired={true}>
-   <div class="bg-red-300">aslkfamfoasdiownednciaosndoasfnas </div>
-   <div>asfnladmfpamsfl asd</div>
-   <div>aslkfasf;,;</div>
-   <button class="btn btn-soft" >Open</button>
-   <button class="btn btn-soft">Open</button>
-</Drawer>
+<ButtonAction cb={async() => await(goto("/onboarding"))}>Onboarding Flow</ButtonAction>
---
+++ infrastructure/eid-wallet/src/routes/onboarding/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+</script>
+
+<main class="pt-[3vh] px-[6vw]">
+    <article class="flex justify-center mb-[4vh]">
+        <img src="/images/Onboarding.svg" alt="card">
+    </article>
+    <section class="mb-[9.3vh]">
+        <h1 class="text-[42px] text-black font-medium">Your <br>
+            Digital Identity,
+            Secured</h1>
+        <p class="text-base text-black-700 font-normal">Store your IDs, verify instantly with QR codes, and manage your digital identity with ease.</p>
+    </section>
+    <section>
+        <p>By continuing you agree to our <a href="/">Terms & Conditions </a> and <a href="/">privacy policy.</a></p>
+    </section>
+</main>
