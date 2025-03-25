d37ddd3086fe1f8717832e3378b52cb9d8b1e4d4
gourav <gourav@auvo.io>
2025-03-25 14:48:28 +0530

    fix: line height and added handlers

--- infrastructure/eid-wallet/src/routes/onboarding/+page.svelte
+++ infrastructure/eid-wallet/src/routes/onboarding/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+    import ButtonAction from "$lib/ui/Button/ButtonAction.svelte";
 
+    const handleLogin = async() => {
+        // login fucntionality
+    }
+
+    const handleGetStarted = async() => {
+        //get started functionality
+    }
 </script>
 
 <main class="pt-[3vh] px-[6vw]">
@@ -7,12 +15,16 @@
         <img src="/images/Onboarding.svg" alt="card">
     </article>
     <section class="mb-[9.3vh]">
-        <h1 class="text-[42px] text-black font-medium">Your <br>
+        <h1 class="text-[42px]/[1] text-black font-medium mb-[1vh]">Your <br>
             Digital Identity,
             Secured</h1>
         <p class="text-base text-black-700 font-normal">Store your IDs, verify instantly with QR codes, and manage your digital identity with ease.</p>
     </section>
     <section>
         <p>By continuing you agree to our <a href="/">Terms & Conditions </a> and <a href="/">privacy policy.</a></p>
+        <div class="grid grid-cols-2 items-center gap-[11px] whitespace-nowrap mt-[1.2vh]">
+            <ButtonAction cb={handleLogin}>Log in</ButtonAction>
+            <ButtonAction cb={handleGetStarted}>Get Started</ButtonAction>
+        </div>
     </section>
 </main>
