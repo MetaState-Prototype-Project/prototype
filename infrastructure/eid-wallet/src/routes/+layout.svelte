a534e36681c09f11e15268e18210007a9a7278b4
gourav <gourav@auvo.io>
2025-03-25 11:19:18 +0530

    feat: SplashScreen

--- infrastructure/eid-wallet/src/routes/+layout.svelte
+++ infrastructure/eid-wallet/src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
     import "../app.css"
     const { children } = $props();
+
+    let showSplashScreen = $state(true);
+
+    $effect(() => {
+		setTimeout(() => {
+			showSplashScreen = false;
+		}, 500);
+	});
 </script>
 
-{@render children?.()}
+{#if showSplashScreen}
+    <div class="flex items-center justify-center">
+        <img class="w-full object-cover" src="/images/SplashScreen.svg" alt="splash-screen">
+    </div>
+{:else}
+    {@render children?.()}
+{/if}
