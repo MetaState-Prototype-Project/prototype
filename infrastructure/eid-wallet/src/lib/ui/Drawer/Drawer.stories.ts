80fe7a2c8cd4d120d010979b4a1aaf4a8008516b
JulienAuvo <julien@auvo.io>
2025-03-26 15:08:18 +0300

    fix: Check Lint / lint

--- infrastructure/eid-wallet/src/lib/ui/Drawer/Drawer.stories.ts
+++ infrastructure/eid-wallet/src/lib/ui/Drawer/Drawer.stories.ts
@@ -1,11 +1,12 @@
 import Drawer from './Drawer.svelte'
 import { InnerContent } from './Drawer.stories.snippet.svelte'
+import type { Snippet } from 'svelte'
 
 export default {
   title: 'UI/Drawer',
   component: Drawer,
   tags: ['autodocs'],
-  render: (args: { isPaneOpen: boolean; children: any }) => ({
+  render: (args: { isPaneOpen: boolean; children: Snippet }) => ({
     Component: Drawer,
     props: args,
   }),
