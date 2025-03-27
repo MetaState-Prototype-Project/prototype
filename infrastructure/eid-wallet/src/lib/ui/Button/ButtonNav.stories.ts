70b87a0e1aed5db3ca58364ad128ebfc00ded2e4
JulienAuvo <julien@auvo.io>
2025-03-27 15:30:18 +0300

    chore: format

--- infrastructure/eid-wallet/src/lib/ui/Button/ButtonNav.stories.ts
+++ infrastructure/eid-wallet/src/lib/ui/Button/ButtonNav.stories.ts
@@ -1,27 +1,27 @@
 import type { ComponentProps } from "svelte";
 import ButtonNav from "./ButtonNav.svelte";
 import {
-    ButtonNavSettings,
-    ButtonNavText,
+	ButtonNavSettings,
+	ButtonNavText,
 } from "./Button.stories.snippet.svelte";
 
 export default {
-    title: "UI/ButtonNav",
-    component: ButtonNav,
-    tags: ["autodocs"],
-    render: (args: {
-        Component: ButtonNav;
-        props: ComponentProps<typeof ButtonNav>;
-    }) => ({
-        Component: ButtonNav,
-        props: args,
-    }),
+	title: "UI/ButtonNav",
+	component: ButtonNav,
+	tags: ["autodocs"],
+	render: (args: {
+		Component: ButtonNav;
+		props: ComponentProps<typeof ButtonNav>;
+	}) => ({
+		Component: ButtonNav,
+		props: args,
+	}),
 };
 
 export const Default = {
-    args: { href: "#", children: ButtonNavText },
+	args: { href: "#", children: ButtonNavText },
 };
 
 export const ForSettings = {
-    args: { href: "#", children: ButtonNavSettings },
+	args: { href: "#", children: ButtonNavSettings },
 };
