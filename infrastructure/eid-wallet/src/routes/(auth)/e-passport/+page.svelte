7805e7def0c401bee0fdeda55ebe4ea5bf316e4c
gourav <gourav@auvo.io>
2025-03-27 14:46:11 +0530

    fix: identity card implementation

--- infrastructure/eid-wallet/src/routes/(auth)/e-passport/+page.svelte
+++ infrastructure/eid-wallet/src/routes/(auth)/e-passport/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
+import IdentityCard from "$lib/fragments/IdentityCard/IdentityCard.svelte";
 import { ButtonAction } from "$lib/ui";
-
-let usedStorage = $state(50);
-let totalStorage = $state(90);
 
 const handleFinish = async () => {};
 </script>
@@ -12,17 +10,21 @@ const handleFinish = async () => {};
     <section>
         <h3 class="mb-[0.5vh]">Your ePassport and eVault are ready</h3>
         <p class="text-black-700! mb-[3vh]">Log into any W3DS platform without passwords. It’s tied to this phone; if lost, you’ll need to revoke and reissue it on a new device.</p>
+        <IdentityCard variant= "ePassport"
+		viewBtn= {() => alert("View")}
+		userData= {{
+			Name: "Ananya",
+			Dob: "29 Nov 2003",
+			Nationality: "Indian",
+			Passport: "234dfvgsdfg",
+		}}/>
     </section>
-    <section>
-        <h3 class="mb-[0.5vh]">Your eVault</h3>
+    <section class="mt-[4vh]">
+        <h4 class="mb-[0.5vh]">Your eVault</h4>
         <p class="text-black-700! mb-[3vh]">We’ve also created your eVault—secure cloud storage for your personal data. W3DS platforms access it directly, keeping you in control.</p>
-    </section>
-    <section class="bg-gray rounded-[24px] p-[24px]">
-        <div class="flex justify-between items-center mb-[12px]">
-            <p>{usedStorage}GB used</p>
-            <p>{totalStorage}GB total storage</p>
-        </div>
-        <progress class="progress progress-secondary bg-primary-400 w-full h-[16px] rounded-xl" value={(usedStorage/totalStorage)*100} max="100"></progress>
+        <IdentityCard variant= "eVault"
+		usedStorage= {15}
+		totalStorage= {80}/>
     </section>
     <ButtonAction class="w-full" callback={handleFinish}>Finish</ButtonAction>
 </main>
--- infrastructure/eid-wallet/src/routes/(auth)/review/+page.svelte
+++ infrastructure/eid-wallet/src/routes/(auth)/review/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { goto } from "$app/navigation";
-import { IdentifierCard } from "$lib/fragments";
+import { IdentityCard } from "$lib/fragments";
 import { ButtonAction } from "$lib/ui";
 
 const handleNext = async () => {
@@ -13,7 +13,7 @@ const handleNext = async () => {
         <h3 class="mb-[0.5vh]">Here’s your eName</h3>
         <p class="text-black-700!  mb-[3vh]">This identifier is permanently yours, and it stays with you for your whole life.</p>
         
-        <IdentifierCard eName="AbcXyz" code="@asdasd-asdafaf-asdaffa-asfafaf"/>
+        <IdentityCard variant="eName" userId= "acbsdjk-14n14k43-12412" shareBtn={() => null}  viewBtn={() => null}/>
     </section>
     <ButtonAction class="w-full" callback={handleNext}>Next</ButtonAction>
 </main>
