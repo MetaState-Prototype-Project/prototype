ce34b329b73f3d42ec9db9a0568fdf372482aac3
JulienAuvo <julien@auvo.io>
2025-03-27 23:04:40 +0300

    chore: remove useless change

--- infrastructure/eid-wallet/src/routes/+page.svelte
+++ infrastructure/eid-wallet/src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-import Header from "$lib/fragments/Header/Header.svelte";
-import IdentityCard from "$lib/fragments/IdentityCard/IdentityCard.svelte";
-function handleProfile() {
-	console.log("click");
-}
 </script>
 
-<Header title="Identity Card"  handleProfile={handleProfile} />
-
-<IdentityCard viewBtn={() => console.log("click")} />
+<div class="w-screen h-6 bg-red-100">red 100</div>
+<div class="w-screen h-6 bg-danger-100">danger 100</div>
+<div class="w-screen h-6 bg-red-200">red 200</div>
+<div class="w-screen h-6 bg-red-300">red 300</div>
+<div class="w-screen h-6 bg-danger-300">danger 300</div>
+<div class="w-screen h-6 bg-red-400">red 400</div>
+<div class="w-screen h-6 bg-red-500">red 500</div>
+<div class="w-screen h-6 bg-danger-500">danger 500</div>
