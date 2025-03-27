a17fd18a985ee464ad54f2d6088ed1e53942bc8d
JulienAuvo <julien@auvo.io>
2025-03-27 22:58:30 +0300

    feat: refactor ButtonIcon to be used anywhere in the app

--- infrastructure/eid-wallet/src/routes/+page.svelte
+++ infrastructure/eid-wallet/src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+import Header from "$lib/fragments/Header/Header.svelte";
 import IdentityCard from "$lib/fragments/IdentityCard/IdentityCard.svelte";
+function handleProfile() {
+	console.log("click");
+}
 </script>
+
+<Header title="Identity Card"  handleProfile={handleProfile} />
 
 <IdentityCard viewBtn={() => console.log("click")} />
