4370d1cd9a93712e9f16d9b886069b79b3ec4c9c
JulienAuvo <julien@auvo.io>
2025-03-28 15:30:23 +0300

    feat: add animation to splashScreen

--- infrastructure/eid-wallet/src/routes/+layout.svelte
+++ infrastructure/eid-wallet/src/routes/+layout.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
+import SplashScreen from "$lib/fragments/SplashScreen/SplashScreen.svelte";
+import ButtonAction from "$lib/ui/Button/ButtonAction.svelte";
 import "../app.css";
 const { children } = $props();
 
-let showSplashScreen = $state(true);
+let showSplashScreen = $state(false);
 
 $effect(() => {
+	showSplashScreen = true; // needs to happen this way to kickstart the animation
 	setTimeout(() => {
 		showSplashScreen = false;
-	}, 500);
+	}, 1000);
 });
 </script>
 
 {#if showSplashScreen}
-    <div class="relative w-full h-[100vh] bg-primary">
-        <img class="absolute w-full bottom-[-80px] start-0" src="/images/Shape1.svg" alt="illustration">
-        <img class="absolute w-full top-[50px] end-[100px]" src="/images/Shape2.svg" alt="illustration">
-        <div class="absolute w-full top-[42%] start-[50%] translate-x-[-50%] translate-y-[-42%]">
-            <h1 class="text-center text-white text-[44px]">eAgency</h1>
-            <p class="text-center text-white">Your self-sovereign digital entity</p>
-        </div>
-        <img class="absolute bottom-[116px] start-[50%] translate-x-[-50%]" src="/images/W3DSLogoWhite.svg" alt="logo">
-    </div>
+    <SplashScreen />
 {:else}
     {@render children?.()}
 {/if}
