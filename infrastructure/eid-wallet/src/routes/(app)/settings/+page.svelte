27ed83c5eaeb2dd386d131e068f9bc77657eb1bf
gourav <gourav@auvo.io>
2025-03-31 15:28:37 +0530

    feat: settings and language page

--- infrastructure/eid-wallet/src/routes/(app)/settings/+page.svelte
+++ infrastructure/eid-wallet/src/routes/(app)/settings/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
-
+import { SettingsNavigationBtn } from "$lib/fragments";
+import {
+	Key01Icon,
+	LanguageSquareIcon,
+	Link02Icon,
+	PinCodeIcon,
+	Shield01Icon,
+} from "@hugeicons/core-free-icons";
 </script>
 
-<main>
-    
+<main class="pt-[3vh] px-[5vw] pb-[4.5vh]">
+    <!-- header part -->
+    <SettingsNavigationBtn icon={LanguageSquareIcon} label="Language" href="/settings/language"/>
+    <SettingsNavigationBtn icon={Link02Icon} label="History" href="/settings/history"/>
+    <SettingsNavigationBtn icon={PinCodeIcon} label="Pin" href="/settings/pin"/>
+    <SettingsNavigationBtn icon={Key01Icon} label="Keys" href="/settings/keys"/>
+    <SettingsNavigationBtn icon={Shield01Icon} label="Privacy" href="/settings/privacy"/>
 </main>
---
+++ infrastructure/eid-wallet/src/routes/(app)/settings/language/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { Selector } from "$lib/ui";
+
+let languages: { name: string; country: string }[] = [
+	{ name: "English", country: "gb" },
+	{ name: "Spanish", country: "es" },
+	{ name: "German", country: "de" },
+	{ name: "French", country: "fr" },
+];
+let selected = $state("");
+</script>
+
+
+
+<main class="pt-[3vh] px-[5vw] pb-[4.5vh]">
+    {#each languages as lang, i}
+    <Selector id={`option-${i}`} name={lang.name} bind:selected value={lang.name}>
+        {lang.name}
+        {#snippet icon()}
+        <div
+            class={`rounded-full fi fis fi-${lang.country} scale-150 mr-12 outline-8 outline-gray`}
+        ></div>
+        {/snippet}
+    </Selector>
+    {/each}
+</main>
