4ed143b7b2ca30203a8ff81a495c05a31df59fc8
Merul Dhiman <me@merul.org>
2025-04-01 12:31:26 +0530

    feat: add storage specification abstract class

---
+++ infrastructure/w3id/src/logs/storage/storage-spec.ts
@@ -0,0 +1,43 @@
+/**
+ * SPECIFICATION
+ *
+ * Baseline Storage Specification, used for storing and managing logs
+ */
+
+export declare class StorageSpec<T, K> {
+  /**
+   * Build a new storage driver
+   *
+   * @param {...any[]} props
+   * @returns Promise<StorageSpec>
+   */
+
+  public static build<T, K>(...props: any[]): Promise<StorageSpec<T, K>>;
+
+  /**
+   * Create a new entry in the storage
+   *
+   * @param body
+   * @returns Promise<K>
+   */
+
+  public create(body: T): Promise<K>;
+
+  /**
+   * Find one entry in the storage using partial of K
+   *
+   * @param {Partial<K>} options
+   * @returns Promise<K>
+   */
+
+  public findOne(options: Partial<K>): Promise<K>;
+
+  /**
+   * Find many entities in the storage using partial of K
+   *
+   * @param {Partial<K>} options
+   * @returns Promise<K[]>
+   */
+
+  public findMany(options: Partial<K>): Promise<K[]>;
+}
