f2f6473cf51d5705c7b9b07d7f02169e92cde7c9
gourav <gourav@auvo.io>
2025-04-01 16:08:54 +0530

    chore: used app nav component

--- infrastructure/eid-wallet/src/routes/(app)/settings/+layout.svelte
+++ infrastructure/eid-wallet/src/routes/(app)/settings/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-import { Header } from "$lib/fragments";
+import { AppNav } from "$lib/fragments";
 import { runtime } from "$lib/global/runtime.svelte";
 
 const { children } = $props();
 </script>
 
 <main class="h-[100vh] px-[5vw] pb-[4.5vh]">
-    <Header title={runtime.header.title ?? ""}/>
+    <AppNav title={runtime.header.title ?? ""}/>
     {@render children?.()}
 </main>
