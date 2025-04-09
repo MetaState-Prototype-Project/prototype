3fc21b612755f97a6f2bcb2fcd81081261683bba
Merul Dhiman <me@merul.org>
2025-04-09 14:07:08 +0530

    chore: add missing file

--- infrastructure/w3id/src/logs/log.types.ts
+++ infrastructure/w3id/src/logs/log.types.ts
@@ -1,35 +1,41 @@
 export type LogEvent = {
-	id: string;
-	versionId: string;
-	versionTime: Date;
-	updateKeys: string[];
-	nextKeyHashes: string[];
-	method: `w3id:v${string}`;
-	proof?: string;
+    id: string;
+    versionId: string;
+    versionTime: Date;
+    updateKeys: string[];
+    nextKeyHashes: string[];
+    method: `w3id:v${string}`;
+    proof?: string;
 };
 
 export enum LogEvents {
-	Rotation = 0,
-	Genesis = 1,
+    Rotation,
+    Genesis,
 }
 
 export type RotationLogOptions = {
-	type: LogEvents.Rotation;
-	nextKeyHashes: string[];
-	signer: Signer;
-	nextKeySigner: Signer;
+    type: LogEvents.Rotation;
+    nextKeyHashes: string[];
+    signer: Signer;
+    nextKeySigner: Signer;
 };
 
+export type VerifierCallback = (
+    message: string,
+    signature: string,
+    pubKey: string,
+) => Promise<boolean>;
+
 export type GenesisLogOptions = {
-	type: LogEvents.Genesis;
-	nextKeyHashes: string[];
-	id: string;
-	signer: Signer;
+    type: LogEvents.Genesis;
+    nextKeyHashes: string[];
+    id: string;
+    signer: Signer;
 };
 
 export type Signer = {
-	sign: (string: string) => Promise<string> | string;
-	pubKey: string;
+    sign: (string: string) => Promise<string> | string;
+    pubKey: string;
 };
 
 export type CreateLogEventOptions = GenesisLogOptions | RotationLogOptions;
