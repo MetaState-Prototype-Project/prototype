82bce445e0966e11610cbace940927be56c54db8
Merul Dhiman <me@merul.org>
2025-04-09 14:09:23 +0530

    chore: format

--- infrastructure/w3id/src/logs/log-manager.ts
+++ infrastructure/w3id/src/logs/log-manager.ts
@@ -1,18 +1,18 @@
 import canonicalize from "canonicalize";
 import {
-    BadSignatureError,
-    MalformedHashChainError,
-    MalformedIndexChainError,
+	BadSignatureError,
+	MalformedHashChainError,
+	MalformedIndexChainError,
 } from "../errors/errors";
 import { isSubsetOf } from "../utils/array";
 import { hash } from "../utils/hash";
 import {
-    type CreateLogEventOptions,
-    type GenesisLogOptions,
-    type LogEvent,
-    LogEventType,
-    type RotationLogOptions,
-    type VerifierCallback,
+	type CreateLogEventOptions,
+	type GenesisLogOptions,
+	type LogEvent,
+	LogEventType,
+	type RotationLogOptions,
+	type VerifierCallback,
 } from "./log.types";
 import type { StorageSpec } from "./storage/storage-spec";
 
@@ -24,123 +24,118 @@ import type { StorageSpec } from "./storage/storage-spec";
 // TODO: Create a specification link inside our docs for how generation of identifier works
 
 export class IDLogManager {
-    repository: StorageSpec<LogEvent, LogEvent>;
+	repository: StorageSpec<LogEvent, LogEvent>;
 
-    constructor(repository: StorageSpec<LogEvent, LogEvent>) {
-        this.repository = repository;
-    }
+	constructor(repository: StorageSpec<LogEvent, LogEvent>) {
+		this.repository = repository;
+	}
 
-    static async validateLogChain(
-        log: LogEvent[],
-        verifyCallback: VerifierCallback,
-    ) {
-        let currIndex = 0;
-        let currentNextKeyHashesSeen: string[] = [];
-        let lastUpdateKeysSeen: string[] = [];
-        let lastHash: string | null = null;
+	static async validateLogChain(
+		log: LogEvent[],
+		verifyCallback: VerifierCallback,
+	) {
+		let currIndex = 0;
+		let currentNextKeyHashesSeen: string[] = [];
+		let lastUpdateKeysSeen: string[] = [];
+		let lastHash: string | null = null;
 
-        for (const e of log) {
-            const [_index, _hash] = e.versionId.split("-");
-            const index = Number(_index);
-            if (currIndex !== index) throw new MalformedIndexChainError();
-            const hashedUpdateKeys = await Promise.all(
-                e.updateKeys.map(async (k) => await hash(k)),
-            );
-            if (index > 0) {
-                const updateKeysSeen = isSubsetOf(
-                    hashedUpdateKeys,
-                    currentNextKeyHashesSeen,
-                );
-                if (!updateKeysSeen || lastHash !== _hash)
-                    throw new MalformedHashChainError();
-            }
+		for (const e of log) {
+			const [_index, _hash] = e.versionId.split("-");
+			const index = Number(_index);
+			if (currIndex !== index) throw new MalformedIndexChainError();
+			const hashedUpdateKeys = await Promise.all(
+				e.updateKeys.map(async (k) => await hash(k)),
+			);
+			if (index > 0) {
+				const updateKeysSeen = isSubsetOf(
+					hashedUpdateKeys,
+					currentNextKeyHashesSeen,
+				);
+				if (!updateKeysSeen || lastHash !== _hash)
+					throw new MalformedHashChainError();
+			}
 
-            currentNextKeyHashesSeen = e.nextKeyHashes;
-            await IDLogManager.verifyLogEventProof(
-                e,
-                lastUpdateKeysSeen.length > 0
-                    ? lastUpdateKeysSeen
-                    : e.updateKeys,
-                verifyCallback,
-            );
-            lastUpdateKeysSeen = e.updateKeys;
-            currIndex++;
-            lastHash = await hash(canonicalize(e) as string);
-        }
-        return true;
-    }
+			currentNextKeyHashesSeen = e.nextKeyHashes;
+			await IDLogManager.verifyLogEventProof(
+				e,
+				lastUpdateKeysSeen.length > 0 ? lastUpdateKeysSeen : e.updateKeys,
+				verifyCallback,
+			);
+			lastUpdateKeysSeen = e.updateKeys;
+			currIndex++;
+			lastHash = await hash(canonicalize(e) as string);
+		}
+		return true;
+	}
 
-    private static async verifyLogEventProof(
-        e: LogEvent,
-        currentUpdateKeys: string[],
-        verifyCallback: VerifierCallback,
-    ) {
-        const proof = e.proof;
-        const copy = JSON.parse(JSON.stringify(e));
-        // biome-ignore lint/performance/noDelete: we need to delete proof completely
-        delete copy.proof;
-        const canonicalJson = canonicalize(copy);
-        let verified = false;
-        if (!proof) throw new Error();
-        for (const key of currentUpdateKeys) {
-            const signValidates = await verifyCallback(
-                canonicalJson as string,
-                proof,
-                key,
-            );
-            if (signValidates) verified = true;
-        }
-        if (!verified) throw new BadSignatureError();
-    }
+	private static async verifyLogEventProof(
+		e: LogEvent,
+		currentUpdateKeys: string[],
+		verifyCallback: VerifierCallback,
+	) {
+		const proof = e.proof;
+		const copy = JSON.parse(JSON.stringify(e));
+		// biome-ignore lint/performance/noDelete: we need to delete proof completely
+		delete copy.proof;
+		const canonicalJson = canonicalize(copy);
+		let verified = false;
+		if (!proof) throw new Error();
+		for (const key of currentUpdateKeys) {
+			const signValidates = await verifyCallback(
+				canonicalJson as string,
+				proof,
+				key,
+			);
+			if (signValidates) verified = true;
+		}
+		if (!verified) throw new BadSignatureError();
+	}
 
-    private async appendEntry(
-        entries: LogEvent[],
-        options: RotationLogOptions,
-    ) {
-        const { signer, nextKeyHashes, nextKeySigner } = options;
-        const latestEntry = entries[entries.length - 1];
-        const logHash = await hash(latestEntry);
-        const index = Number(latestEntry.versionId.split("-")[0]) + 1;
+	private async appendEntry(entries: LogEvent[], options: RotationLogOptions) {
+		const { signer, nextKeyHashes, nextKeySigner } = options;
+		const latestEntry = entries[entries.length - 1];
+		const logHash = await hash(latestEntry);
+		const index = Number(latestEntry.versionId.split("-")[0]) + 1;
 
-        const currKeyHash = await hash(nextKeySigner.pubKey);
-        if (!latestEntry.nextKeyHashes.includes(currKeyHash)) throw new Error();
+		const currKeyHash = await hash(nextKeySigner.pubKey);
+		if (!latestEntry.nextKeyHashes.includes(currKeyHash)) throw new Error();
 
-        const logEvent: LogEvent = {
-            id: latestEntry.id,
-            versionTime: new Date(Date.now()),
-            versionId: `${index}-${logHash}`,
-            updateKeys: [nextKeySigner.pubKey],
-            nextKeyHashes: nextKeyHashes,
-            method: "w3id:v0.0.0",
-        };
+		const logEvent: LogEvent = {
+			id: latestEntry.id,
+			versionTime: new Date(Date.now()),
+			versionId: `${index}-${logHash}`,
+			updateKeys: [nextKeySigner.pubKey],
+			nextKeyHashes: nextKeyHashes,
+			method: "w3id:v0.0.0",
+		};
 
-        const proof = await signer.sign(canonicalize(logEvent) as string);
-        logEvent.proof = proof;
+		const proof = await signer.sign(canonicalize(logEvent) as string);
+		logEvent.proof = proof;
 
-        await this.repository.create(logEvent);
-        return logEvent;
-    }
+		await this.repository.create(logEvent);
+		return logEvent;
+	}
 
-    private async createGenesisEntry(options: GenesisLogOptions) {
-        const { id, nextKeyHashes, signer } = options;
-        const logEvent: LogEvent = {
-            id,
-            versionId: `0-${id.split("@")[1]}`,
-            versionTime: new Date(Date.now()),
-            updateKeys: [signer.pubKey],
-            nextKeyHashes: nextKeyHashes,
-            method: "w3id:v0.0.0",
-        };
-        const proof = await signer.sign(canonicalize(logEvent) as string);
-        logEvent.proof = proof;
-        await this.repository.create(logEvent);
-        return logEvent;
-    }
+	private async createGenesisEntry(options: GenesisLogOptions) {
+		const { id, nextKeyHashes, signer } = options;
+		const logEvent: LogEvent = {
+			id,
+			versionId: `0-${id.split("@")[1]}`,
+			versionTime: new Date(Date.now()),
+			updateKeys: [signer.pubKey],
+			nextKeyHashes: nextKeyHashes,
+			method: "w3id:v0.0.0",
+		};
+		const proof = await signer.sign(canonicalize(logEvent) as string);
+		logEvent.proof = proof;
+		await this.repository.create(logEvent);
+		return logEvent;
+	}
 
-    async createLogEvent(options: CreateLogEventOptions) {
-        const entries = await this.repository.findMany({});
-        if (options.type === LogEventType.Genesis)
-            return this.createGenesisEntry(options);
-        return this.appendEntry(entries, options);
-    }
+	async createLogEvent(options: CreateLogEventOptions) {
+		const entries = await this.repository.findMany({});
+		if (options.type === LogEventType.Genesis)
+			return this.createGenesisEntry(options);
+		return this.appendEntry(entries, options);
+	}
 }
--- infrastructure/w3id/src/logs/log.types.ts
+++ infrastructure/w3id/src/logs/log.types.ts
@@ -1,41 +1,41 @@
 export type LogEvent = {
-    id: string;
-    versionId: string;
-    versionTime: Date;
-    updateKeys: string[];
-    nextKeyHashes: string[];
-    method: `w3id:v${string}`;
-    proof?: string;
+	id: string;
+	versionId: string;
+	versionTime: Date;
+	updateKeys: string[];
+	nextKeyHashes: string[];
+	method: `w3id:v${string}`;
+	proof?: string;
 };
 
 export enum LogEventType {
-    Rotation,
-    Genesis,
+	Rotation,
+	Genesis,
 }
 
 export type RotationLogOptions = {
-    type: LogEventType.Rotation;
-    nextKeyHashes: string[];
-    signer: Signer;
-    nextKeySigner: Signer;
+	type: LogEventType.Rotation;
+	nextKeyHashes: string[];
+	signer: Signer;
+	nextKeySigner: Signer;
 };
 
 export type VerifierCallback = (
-    message: string,
-    signature: string,
-    pubKey: string,
+	message: string,
+	signature: string,
+	pubKey: string,
 ) => Promise<boolean>;
 
 export type GenesisLogOptions = {
-    type: LogEventType.Genesis;
-    nextKeyHashes: string[];
-    id: string;
-    signer: Signer;
+	type: LogEventType.Genesis;
+	nextKeyHashes: string[];
+	id: string;
+	signer: Signer;
 };
 
 export type Signer = {
-    sign: (string: string) => Promise<string> | string;
-    pubKey: string;
+	sign: (string: string) => Promise<string> | string;
+	pubKey: string;
 };
 
 export type CreateLogEventOptions = GenesisLogOptions | RotationLogOptions;
