f3b38f19a9a3afe003cceed109ccd7f3ee98f9d5
Soham Jaiswal <sosweetham@gmail.com>
2025-04-09 10:45:11 +0530

    feat: tauri plugins setup

---
+++ infrastructure/eid-wallet/src/lib/global/state.ts
@@ -0,0 +1,35 @@
+import { Store } from "@tauri-apps/plugin-store";
+
+/**
+ * @author SoSweetHam
+ * @description A centralized state that can be used to control the global state of the application, meant to be used as a singleton through the  main layout component.
+ *
+ * You cannot use this class directly, instead use the `GlobalState.create()` method to get an instance.
+ */
+export class GlobalState {
+  #store: Store;
+  private constructor(store: Store) {
+    this.#store = store;
+  }
+
+  /**
+   * @author SoSweetHam
+   * @description Creator of the GlobalState singleton
+   * @returns A promise of a new instance of GlobalState
+   */
+  static async create() {
+    const defaultTheme = "system";
+
+    const store = await Store.load("global-state.json", {
+      autoSave: true,
+    });
+    const alreadyInitialized = await store.get<boolean>("initialized");
+
+    const instance = new GlobalState(store);
+
+    if (!alreadyInitialized) {
+      await instance.#store.set("initialized", true);
+    }
+    return instance;
+  }
+}
