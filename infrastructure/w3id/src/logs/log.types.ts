9e0580a743aa8db3448c4d330cf6a91e40e64066
Merul Dhiman <me@merul.org>
2025-04-16 22:59:32 +0530

    chore: add docs

--- infrastructure/w3id/src/logs/log.types.ts
+++ infrastructure/w3id/src/logs/log.types.ts
@@ -1,43 +1,43 @@
 export type LogEvent = {
-    id: string;
-    versionId: string;
-    versionTime: Date;
-    updateKeys: string[];
-    nextKeyHashes: string[];
-    method: `w3id:v${string}`;
-    proof?: string;
+	id: string;
+	versionId: string;
+	versionTime: Date;
+	updateKeys: string[];
+	nextKeyHashes: string[];
+	method: `w3id:v${string}`;
+	proof?: string;
 };
 
 export type VerifierCallback = (
-    message: string,
-    signature: string,
-    pubKey: string,
+	message: string,
+	signature: string,
+	pubKey: string,
 ) => Promise<boolean>;
 
 export type Signer = {
-    sign: (message: string) => Promise<string> | string;
-    pubKey: string;
+	sign: (message: string) => Promise<string> | string;
+	pubKey: string;
 };
 
 export type RotationLogOptions = {
-    nextKeyHashes: string[];
-    nextKeySigner: Signer;
+	nextKeyHashes: string[];
+	nextKeySigner: Signer;
 };
 
 export type GenesisLogOptions = {
-    nextKeyHashes: string[];
-    id: string;
+	nextKeyHashes: string[];
+	id: string;
 };
 
 export function isGenesisOptions(
-    options: CreateLogEventOptions,
+	options: CreateLogEventOptions,
 ): options is GenesisLogOptions {
-    return "id" in options;
+	return "id" in options;
 }
 export function isRotationOptions(
-    options: CreateLogEventOptions,
+	options: CreateLogEventOptions,
 ): options is RotationLogOptions {
-    return "nextKeySigner" in options;
+	return "nextKeySigner" in options;
 }
 
 export type CreateLogEventOptions = GenesisLogOptions | RotationLogOptions;
--- infrastructure/w3id/src/utils/rand.ts
+++ infrastructure/w3id/src/utils/rand.ts
@@ -5,15 +5,15 @@
  * @returns {string}
  */
 
-export function generateRandomAlphaNum(length: number = 16): string {
-    const chars =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let result = "";
-    const charsLength = chars.length;
+export function generateRandomAlphaNum(length = 16): string {
+	const chars =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let result = "";
+	const charsLength = chars.length;
 
-    for (let i = 0; i < length; i++) {
-        result += chars.charAt(Math.floor(Math.random() * charsLength));
-    }
+	for (let i = 0; i < length; i++) {
+		result += chars.charAt(Math.floor(Math.random() * charsLength));
+	}
 
-    return result;
+	return result;
 }
