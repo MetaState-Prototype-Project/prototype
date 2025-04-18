b1ccbb5bf1383ddcd3496566dac01246a5ae582d
Merul Dhiman <me@merul.org>
2025-04-19 04:00:22 +0530

    chore: format

--- infrastructure/w3id/src/logs/log.types.ts
+++ infrastructure/w3id/src/logs/log.types.ts
@@ -1,61 +1,61 @@
 export type LogEvent = {
-    id: string;
-    versionId: string;
-    versionTime: Date;
-    updateKeys: string[];
-    nextKeyHashes: string[];
-    method: `w3id:v${string}`;
-    proof?: string;
+	id: string;
+	versionId: string;
+	versionTime: Date;
+	updateKeys: string[];
+	nextKeyHashes: string[];
+	method: `w3id:v${string}`;
+	proof?: string;
 };
 
 export type VerifierCallback = (
-    message: string,
-    signature: string,
-    pubKey: string,
+	message: string,
+	signature: string,
+	pubKey: string,
 ) => Promise<boolean>;
 
 export type JWTHeader = {
-    alg: string;
-    typ: "JWT";
-    kid?: string;
+	alg: string;
+	typ: "JWT";
+	kid?: string;
 };
 
 export type JWTPayload = {
-    [key: string]: any;
-    iat?: number;
-    exp?: number;
-    nbf?: number;
-    iss?: string;
-    sub?: string;
-    aud?: string;
-    jti?: string;
+	[key: string]: unknown;
+	iat?: number;
+	exp?: number;
+	nbf?: number;
+	iss?: string;
+	sub?: string;
+	aud?: string;
+	jti?: string;
 };
 
 export type Signer = {
-    sign: (message: string) => Promise<string> | string;
-    pubKey: string;
-    alg: string;
+	sign: (message: string) => Promise<string> | string;
+	pubKey: string;
+	alg: string;
 };
 
 export type RotationLogOptions = {
-    nextKeyHashes: string[];
-    nextKeySigner: Signer;
+	nextKeyHashes: string[];
+	nextKeySigner: Signer;
 };
 
 export type GenesisLogOptions = {
-    nextKeyHashes: string[];
-    id: string;
+	nextKeyHashes: string[];
+	id: string;
 };
 
 export function isGenesisOptions(
-    options: CreateLogEventOptions,
+	options: CreateLogEventOptions,
 ): options is GenesisLogOptions {
-    return "id" in options;
+	return "id" in options;
 }
 export function isRotationOptions(
-    options: CreateLogEventOptions,
+	options: CreateLogEventOptions,
 ): options is RotationLogOptions {
-    return "nextKeySigner" in options;
+	return "nextKeySigner" in options;
 }
 
 export type CreateLogEventOptions = GenesisLogOptions | RotationLogOptions;
