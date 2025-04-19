c9e6dce081a86d09994888dd007112b4044c7578
Merul Dhiman <me@merul.org>
2025-04-19 22:40:11 +0530

    chore: stash dockerfile progress

--- infrastructure/evault-core/src/evault.ts
+++ infrastructure/evault-core/src/evault.ts
@@ -1,12 +1,9 @@
-import neo4j from "neo4j-driver";
 import { DbService } from "./db/db.service";
 import { GraphQLServer } from "./protocol/graphql-server";
 
 async function startEVault() {
-    const uri = `bolt://localhost:7687`;
-    const driver = neo4j.driver(uri, neo4j.auth.basic("neo4j", "testpass"));
-    const dbService = new DbService(driver);
-    new GraphQLServer(dbService);
+  const dbService = new DbService();
+  new GraphQLServer(dbService);
 }
 
 startEVault();
