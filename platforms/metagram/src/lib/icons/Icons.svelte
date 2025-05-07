b13d8a11e756a5b633f35149f5043422125d3d80
gourav <gourav@auvo.io>
2025-05-07 11:25:16 +0530

    feat: icons and story file

--- platforms/metagram/src/lib/icons/Icons.svelte
+++ platforms/metagram/src/lib/icons/Icons.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-	import { Like } from ".";
+	import { Like, Refresh, Comment, Home, Flash, CommentsTwo, VerticalDots } from '.';
 </script>
 
-<div class="flex flex-wrap gap-2 items-center">
-    <Like size="20px" color="#A5A5A5"/>
+<div class="flex flex-wrap items-center gap-2">
+	<Like />
+	<Refresh />
+	<Comment />
+	<VerticalDots />
+	<Home />
+	<Flash />
+	<CommentsTwo />
 </div>
