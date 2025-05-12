9437b7ffb3b171755300d6d49b20ad11b4bd7bdb
Sahil Garg <sahil@auvo.io>
2025-05-12 13:26:32 +0530

    chore: made it as it was.

--- platforms/metagram/src/lib/ui/Avatar/Avatar.stories.ts
+++ platforms/metagram/src/lib/ui/Avatar/Avatar.stories.ts
@@ -14,17 +14,17 @@ export default {
     }),
 };
 
-export const Medium = {
-    args: {
-        src: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
-        size: "md",
-    },
-};
-
 export const Large = {
     args: {
         src: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
         size: "lg",
+    },
+};
+
+export const Medium = {
+    args: {
+        src: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        size: "md",
     },
 };
 
