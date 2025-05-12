697172d96d57f91a78930f1994bec1c19aee9a79
gourav <gourav@auvo.io>
2025-05-12 16:56:48 +0530

    feat: Input

---
+++ platforms/metagram/src/lib/ui/Input/Input.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from "svelte/elements";
+
+    interface IInputProps extends HTMLInputAttributes {
+        type: "text" | "number" | "email" | "";
+        value: string | number;
+
+    }
+
+    let {type}:IInputProps = $props();
+</script>
+
+<input {type}>
