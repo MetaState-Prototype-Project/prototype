19777a68d8e5c6922414fd378e12623d6bed1e92
gourav <gourav@auvo.io>
2025-05-15 10:52:59 +0530

    feat: Toggle Switch

--- platforms/metagram/src/lib/ui/Toggle/Toggle.svelte
+++ platforms/metagram/src/lib/ui/Toggle/Toggle.svelte
@@ -10,7 +10,14 @@
 	let uniqueId = Math.random().toString().split('.')[1];
 </script>
 
-<label {...restProps} for={uniqueId} class="relative">
+<label
+	{...restProps}
+	for={uniqueId}
+	class="relative"
+	aria-label={restProps['aria-label'] || 'Toggle'}
+	role="switch"
+	aria-checked={checked}
+>
 	<div class="h-6 w-11 rounded-full bg-gray-200 transition duration-300">
 		<div
 			class={`absolute top-0.5 left-0.5 h-5 w-5 rounded-full ${checked ? 'bg-brand-burnt-orange translate-x-5' : 'translate-x-0 bg-white'} transition duration-300`}
@@ -18,4 +25,4 @@
 	</div>
 </label>
 
-<input id={uniqueId} type="checkbox" bind:checked class="hidden" tabindex="0" />
+<input id={uniqueId} type="checkbox" bind:checked class="hidden" aria-hidden="true" />
