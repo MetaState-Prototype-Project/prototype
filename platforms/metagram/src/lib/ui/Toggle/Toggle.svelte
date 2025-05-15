0a6d011f4746d54fd57793ab113de45636563312
gourav <gourav@auvo.io>
2025-05-15 10:43:46 +0530

    feat: Toggle Switch

---
+++ platforms/metagram/src/lib/ui/Toggle/Toggle.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	interface IToggleProps extends HTMLAttributes<HTMLElement> {
+		checked: boolean;
+	}
+
+	let { checked = $bindable(false), ...restProps }: IToggleProps = $props();
+
+	let uniqueId = Math.random().toString().split('.')[1];
+</script>
+
+<label {...restProps} for={uniqueId} class="relative">
+	<div class="h-6 w-11 rounded-full bg-gray-200 transition duration-300">
+		<div
+			class={`absolute top-0.5 left-0.5 h-5 w-5 rounded-full ${checked ? 'bg-brand-burnt-orange translate-x-5' : 'translate-x-0 bg-white'} transition duration-300`}
+		></div>
+	</div>
+</label>
+
+<input id={uniqueId} type="checkbox" bind:checked class="hidden" tabindex="0" />
