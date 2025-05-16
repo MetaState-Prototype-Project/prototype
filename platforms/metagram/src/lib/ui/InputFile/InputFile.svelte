f6c1ce88923696218c1a98143a010bb2a606e75c
gourav <gourav@auvo.io>
2025-05-16 13:50:38 +0530

    feat: FileInput

---
+++ platforms/metagram/src/lib/ui/InputFile/InputFile.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	interface IInputFileProps {
+		files: FileList | undefined;
+		placeholder: string;
+	}
+
+	let { files = $bindable() }: IInputFileProps = $props();
+
+	const uniqueId = Math.random().toString().split('.')[1];
+
+	$effect(() => {
+		console.log(files);
+	});
+</script>
+
+<input id={uniqueId} type="file" bind:files class="hidden" />
+
+<label
+	for={uniqueId}
+	class="bg-grey text-black-400 font-geist flex h-[158px] w-full items-center justify-center rounded-4xl text-base font-normal"
+>
+	{#if files}
+		<h3 class="text-black-800">{files[0].name.slice(0, 5) + '...'}</h3>
+	{:else}
+		Click to upload a photo
+	{/if}
+</label>
