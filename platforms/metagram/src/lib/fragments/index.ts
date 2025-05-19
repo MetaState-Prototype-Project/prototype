e48e3a2bf5c80ba18877d73c0cf9f251bef77a70
gourav <gourav@auvo.io>
2025-05-19 11:52:06 +0530

    fix: moved to framents

--- platforms/metagram/src/lib/fragments/index.ts
+++ platforms/metagram/src/lib/fragments/index.ts
@@ -1,1 +1,2 @@
 export { default as SettingsNavigationButton } from './SettingsNavigationButton/SettingsNavigationButton.svelte';
+export { default as InputFile } from './InputFile/InputFile.svelte';
--- platforms/metagram/src/lib/ui/index.ts
+++ platforms/metagram/src/lib/ui/index.ts
@@ -1,4 +1,3 @@
 export { default as Button } from './Button/Button.svelte';
 export { default as Avatar } from './Avatar/Avatar.svelte';
 export { default as Input } from './Input/Input.svelte';
-export { default as InputFile } from './InputFile/InputFile.svelte';
