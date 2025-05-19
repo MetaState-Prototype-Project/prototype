8d18eb522f02da6fac67e50f551f4c93817943e2
gourav <gourav@auvo.io>
2025-05-19 15:19:58 +0530

    fix: no need of error and disabled classes

--- platforms/metagram/src/lib/ui/Input/Input.svelte
+++ platforms/metagram/src/lib/ui/Input/Input.svelte
@@ -6,21 +6,17 @@
 		type: 'text' | 'number' | 'email' | 'tel' | 'password';
 		value: string | number;
 		placeholder?: string;
-		disabledClass?: string;
-		errorClass?: string;
 	}
 
 	let {
 		type = 'text',
 		value = $bindable(),
 		placeholder = '',
-		disabledClass = '',
-		errorClass = '',
 		...restProps
 	}: IInputProps = $props();
 
 	const cbase = $derived(
-		`w-full bg-grey py-3.5 px-6 text-[15px] text-black-800 font-geist font-normal placeholder:text-black-600 rounded-4xl outline-0 border border-transparent ${errorClass} ${disabledClass}`
+		`w-full bg-grey py-3.5 px-6 text-[15px] text-black-800 font-geist font-normal placeholder:text-black-600 rounded-4xl outline-0 border border-transparent`
 	);
 </script>
 
