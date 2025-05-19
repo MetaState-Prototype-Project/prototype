aa0a7a6df182326e2111457c091cf5e8f4582e18
gourav <gourav@auvo.io>
2025-05-19 14:41:43 +0530

    fix: as per suggestion

--- platforms/metagram/src/lib/ui/Input/Input.svelte
+++ platforms/metagram/src/lib/ui/Input/Input.svelte
@@ -6,23 +6,21 @@
 		type: 'text' | 'number' | 'email' | 'tel' | 'password';
 		value: string | number;
 		placeholder?: string;
-		isRequired?: boolean;
-		isDisabled?: boolean;
-		isError?: boolean;
+		disabledClass?: string;
+		error?: string;
 	}
 
 	let {
 		type = 'text',
 		value = $bindable(),
 		placeholder = '',
-		isRequired = false,
-		isDisabled = false,
-		isError = false,
+		disabledClass = 'cursor-not-allowed',
+		error = 'border border-red text-red focus:text-black-800 focus:border-transparent',
 		...restProps
 	}: IInputProps = $props();
 
 	const cbase = $derived(
-		`w-full bg-grey py-3.5 px-6 text-[15px] text-black-800 font-geist font-normal placeholder:text-black-600 rounded-4xl outline-0 border border-transparent ${isError && 'border border-red text-red focus:text-black-800 focus:border-transparent'} ${isDisabled && 'cursor-not-allowed'}`
+		`w-full bg-grey py-3.5 px-6 text-[15px] text-black-800 font-geist font-normal placeholder:text-black-600 rounded-4xl outline-0 border border-transparent ${error} ${disabled}`
 	);
 </script>
 
@@ -31,8 +29,6 @@
 	{type}
 	{placeholder}
 	bind:value
-	required={isRequired}
-	disabled={isDisabled}
 	class={cn([cbase, restProps.class].join(' '))}
 	tabindex="0"
 />
