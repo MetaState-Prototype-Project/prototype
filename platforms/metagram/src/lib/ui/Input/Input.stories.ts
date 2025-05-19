c3330069beaaf90ed513629806ceca46a775da3b
Soham Jaiswal <sosweetham@gmail.com>
2025-05-19 17:04:51 +0530

    feat: invalid inputs

--- platforms/metagram/src/lib/ui/Input/Input.stories.ts
+++ platforms/metagram/src/lib/ui/Input/Input.stories.ts
@@ -1,47 +1,50 @@
-import { Input } from '..';
+import { Input } from "..";
 
 export default {
-	title: 'UI/Input',
-	component: Input,
-	tags: ['autodocs'],
-	render: (args: { type: string; placeholder: string; helperText: string }) => ({
-		Component: Input,
-		props: args
-	})
+    title: "UI/Input",
+    component: Input,
+    tags: ["autodocs"],
+    render: (args: {
+        type: string;
+        placeholder: string;
+        helperText: string;
+    }) => ({
+        Component: Input,
+        props: args,
+    }),
 };
 
 export const Text = {
-	args: {
-		type: 'text',
-		placeholder: 'Joe Biden'
-	}
+    args: {
+        type: "text",
+        placeholder: "Joe Biden",
+    },
 };
 
 export const Tel = {
-	args: {
-		type: 'tel',
-		placeholder: '987654321'
-	}
+    args: {
+        type: "tel",
+        placeholder: "987654321",
+    },
 };
 
-export const isError = {
-	args: {
-		type: 'text',
-		placeholder: 'Enter something',
-		errorClass: 'border border-red text-red focus:text-black-800 focus:border-transparent'
-	}
+export const number = {
+    args: {
+        type: "number",
+        placeholder: "Enter something",
+    },
 };
 
 export const Email = {
-	args: {
-		type: 'email',
-		placeholder: 'example@email.com'
-	}
+    args: {
+        type: "email",
+        placeholder: "example@email.com",
+    },
 };
 
 export const Password = {
-	args: {
-		type: 'password',
-		placeholder: 'Please enter password'
-	}
+    args: {
+        type: "password",
+        placeholder: "Please enter password",
+    },
 };
--- platforms/metagram/src/lib/ui/Input/Input.svelte
+++ platforms/metagram/src/lib/ui/Input/Input.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import { cn } from '$lib/utils';
-	import type { HTMLInputAttributes } from 'svelte/elements';
+	import type { HTMLInputAttributes, HTMLInputTypeAttribute } from 'svelte/elements';
 
 	interface IInputProps extends HTMLInputAttributes {
-		type: 'text' | 'number' | 'email' | 'tel' | 'password';
-		value: string | number;
-		placeholder?: string;
+		type: HTMLInputTypeAttribute;
 	}
 
 	let {
@@ -16,7 +14,7 @@
 	}: IInputProps = $props();
 
 	const cbase = $derived(
-		`w-full bg-grey py-3.5 px-6 text-[15px] text-black-800 font-geist font-normal placeholder:text-black-600 rounded-4xl outline-0 border border-transparent`
+		'w-full bg-grey py-3.5 px-6 text-[15px] text-black-800 font-geist font-normal placeholder:text-black-600 rounded-4xl outline-0 border border-transparent focus:text-black-800 focus:border-transparent border invalid:border-red invalid:text-red'
 	);
 </script>
 
