051c6c14fc51dc0eb488c644475e195a68a4a693
gourav <gourav@auvo.io>
2025-05-20 16:09:17 +0530

    fix: modal props

--- platforms/metagram/src/lib/fragments/Modal/Modal.stories.snippet.svelte
+++ platforms/metagram/src/lib/fragments/Modal/Modal.stories.snippet.svelte
@@ -7,12 +7,5 @@
 		Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam rem ipsum possimus maxime
 		modi laborum inventore autem cupiditate, voluptas ex quas, eaque accusantium ipsam natus
 		sapiente libero fuga temporibus eligendi.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam rem ipsum possimus maxime
-		modi laborum inventore autem cupiditate, voluptas ex quas, eaque accusantium ipsam natus
-		sapiente libero fuga temporibus eligendi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam rem ipsum possimus maxime
-		modi laborum inventore autem cupiditate, voluptas ex quas, eaque accusantium ipsam natus
-		sapiente libero fuga temporibus eligendi.Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam rem ipsum possimus maxime
-		modi laborum inventore autem cupiditate, voluptas ex quas, eaque accusantium ipsam natus
-		sapiente libero fuga temporibus eligendi.
 	</p>
 {/snippet}
