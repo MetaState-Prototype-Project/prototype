607ea4e69219bc3fe742553aad88b3ece2d381f1
gourav <gourav@auvo.io>
2025-05-22 10:11:35 +0530

    fix: action menu position

--- platforms/metagram/src/lib/fragments/ActionMenu/ActionMenu.svelte
+++ platforms/metagram/src/lib/fragments/ActionMenu/ActionMenu.svelte
@@ -5,39 +5,40 @@
 	import { tick } from 'svelte';
 	import type { HTMLAttributes } from 'svelte/elements';
 
-	interface IContexMenuProps extends HTMLAttributes<HTMLElement> {
+	interface IContextMenuProps extends HTMLAttributes<HTMLElement> {
 		options: Array<{ name: string; handler: () => void }>;
 	}
 
-	let { options = [], ...restProps }: IContexMenuProps = $props();
+	let { options = [], ...restProps }: IContextMenuProps = $props();
 	let showActionMenu = $state(false);
 	let menuEl: HTMLUListElement | null = $state(null);
-	let mouseX = 0;
-	let mouseY = 0;
+	let buttonEl: HTMLElement | null = null;
 
-	function openMenu(event: MouseEvent) {
-		mouseX = event.clientX;
-		mouseY = event.clientY;
+	function openMenu() {
 		showActionMenu = true;
 
 		tick().then(() => {
-			if (menuEl) {
+			if (menuEl && buttonEl) {
 				const { innerWidth, innerHeight } = window;
-				const rect = menuEl.getBoundingClientRect();
+				const menuRect = menuEl.getBoundingClientRect();
+				const buttonRect = buttonEl.getBoundingClientRect();
 
-				// Adjust horizontal position
-				if (mouseX + rect.width > innerWidth) {
-					menuEl.style.left = `${mouseX - rect.width}px`;
-				} else {
-					menuEl.style.left = `${mouseX}px`;
+				// Position vertically aligned to button top (viewport)
+				let top = buttonRect.top;
+				let left = buttonRect.right;
+
+				// If it overflows right, position to the left of the button
+				if (innerWidth - buttonRect.right < menuRect.width) {
+					left = buttonRect.left - menuRect.width;
 				}
 
-				// Adjust vertical position
-				if (mouseY + rect.height > innerHeight) {
-					menuEl.style.top = `${mouseY - rect.height}px`;
-				} else {
-					menuEl.style.top = `${mouseY}px`;
+				// If it overflows bottom, adjust upward
+				if (innerHeight - buttonRect.top < menuRect.height) {
+					top = innerHeight - menuRect.height - 10;
 				}
+
+				menuEl.style.left = `${left}px`;
+				menuEl.style.top = `${top}px`;
 			}
 		});
 	}
@@ -46,22 +47,39 @@
 		showActionMenu = false;
 	}
 
-	const cBase = 'w-[max-content] py-2 px-5 rounded-2xl bg-white';
+	const cBase = 'w-[max-content] py-2 px-5 rounded-2xl bg-white shadow-lg';
 </script>
 
-<div class={cn([restProps.class].join(' '))}>
-	<button onclick={(e) => {e.preventDefault(), openMenu(e)}}>
-		<HugeiconsIcon icon={MoreVerticalIcon} size={24} color="black"/>
+<div class="relative inline-block">
+	<button
+		bind:this={buttonEl}
+		onclick={(e) => {
+			e.preventDefault(), openMenu();
+		}}
+	>
+		<HugeiconsIcon icon={MoreVerticalIcon} size={24} color="black" />
 	</button>
-	{#if showActionMenu}
-	<ul use:clickOutside={() => closeMenu()} bind:this={menuEl} {...restProps} class={cBase}>
-		{#each options as option}
-		<!-- svelte-ignore a11y_click_events_have_key_events -->
-		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-		<li class="py-3" onclick={() => { option.handler(); closeMenu(); }}>
-			<p class="text-black-800">{option.name}</p>
-		</li>
-	{/each}
-	</ul>
-	{/if}
 </div>
+
+{#if showActionMenu}
+	<ul
+		use:clickOutside={() => closeMenu()}
+		bind:this={menuEl}
+		class={cn([cBase, 'fixed z-50'].join(' '))}
+		style="position: fixed;"
+	>
+		{#each options as option}
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+			<li
+				class="cursor-pointer py-3"
+				onclick={() => {
+					option.handler();
+					closeMenu();
+				}}
+			>
+				<p class="text-black-800">{option.name}</p>
+			</li>
+		{/each}
+	</ul>
+{/if}
