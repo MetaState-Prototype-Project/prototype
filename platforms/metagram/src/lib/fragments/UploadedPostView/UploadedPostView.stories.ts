e8d85284c701fc6056616552569db46613d40318
Sahil Garg <sahil@auvo.io>
2025-06-02 19:44:54 +0530

    feat: added uploaded post view component.

---
+++ platforms/metagram/src/lib/fragments/UploadedPostView/UploadedPostView.stories.ts
@@ -0,0 +1,48 @@
+import type { ComponentProps } from "svelte";
+import UploadedPostView from "./UploadedPostView.svelte";
+
+export default {
+    title: "UI/UploadedPostView",
+    component: UploadedPostView,
+    tags: ["autodocs"],
+    render: (args: {
+        Component: UploadedPostView;
+        props: ComponentProps<typeof UploadedPostView>;
+    }) => ({
+        Component: UploadedPostView,
+        props: args,
+    }),
+};
+
+const IMAGES = [
+    {
+        url: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        alt: "Sample Image 1",
+    },
+    {
+        url: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        alt: "Sample Image 1",
+    },
+    {
+        url: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        alt: "Sample Image 1",
+    },
+    {
+        url: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        alt: "Sample Image 1",
+    },
+    {
+        url: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        alt: "Sample Image 1",
+    },
+    {
+        url: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
+        alt: "Sample Image 1",
+    },
+];
+
+export const Primary = {
+    args: {
+        images: IMAGES,
+    },
+};
---
+++ platforms/metagram/src/lib/fragments/UploadedPostView/UploadedPostView.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { Cross } from '$lib/icons';
+	import { cn } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	interface IImage {
+		url: string;
+		alt: string;
+	}
+
+	interface IUploadedPostViewProps extends HTMLAttributes<HTMLImageElement> {
+		images: IImage[];
+		width?: string;
+		height?: string;
+	}
+
+	let {
+		images,
+		width = 'w-full',
+		height = 'h-full',
+		...restProps
+	}: IUploadedPostViewProps = $props();
+
+	const crossHandler = (i: number) => {
+		images = images.filter((_, index) => index !== i);
+	};
+</script>
+
+<article class="max-w-screen flex flex-row items-center gap-4 scroll-auto">
+	{#each images as image, i}
+		<div class={cn(['relative shrink-0'])}>
+			<Cross
+				class="absolute right-0 top-0 -translate-y-1/2 translate-x-1/2 cursor-pointer"
+				onclick={() => crossHandler(i)}
+			/>
+			<img
+				{...restProps}
+				src={image.url}
+				alt={image.alt}
+				class={cn(['rounded-lg', width, height, restProps.class])}
+			/>
+		</div>
+	{/each}
+</article>
