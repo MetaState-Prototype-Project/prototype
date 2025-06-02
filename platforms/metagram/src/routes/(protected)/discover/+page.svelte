316c4caebc3fae17daec501691ec07f3cd0ef19a
gourav <gourav@auvo.io>
2025-06-02 14:26:49 +0530

    feat: Discover Page

---
+++ platforms/metagram/src/routes/(protected)/discover/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import {Input} from "$lib/ui";
+
+    let searchValue = $state();
+</script>
+
+<section>
+    <Input type="text" bind:value={searchValue} placeholder="Search user, post and more."/>
+</section>
