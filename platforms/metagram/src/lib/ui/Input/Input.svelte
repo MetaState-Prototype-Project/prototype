4249ed2d48d20162f26aa1d3287cfe610c474961
Sahil Garg <sahil@auvo.io>
2025-06-03 15:15:45 +0530

    chore: cleaned the unnecessary classes and variables for input type radio.

--- platforms/metagram/src/lib/ui/Input/Input.svelte
+++ platforms/metagram/src/lib/ui/Input/Input.svelte
@@ -23,16 +23,10 @@
 
 	let radioElement: HTMLInputElement | null = $state(null);
 
-	const typeClasses: Record<string, string> = {
-		radio: 'opacity-100'
-	};
+	const cbase =
+		'w-full bg-grey py-3.5 px-6 text-[15px] text-black-800 font-geist font-normal placeholder:text-black-600 rounded-4xl outline-0 border border-transparent invalid:border-red invalid:text-red focus:invalid:text-black-800 focus:invalid:border-transparent';
 
-	const cbase = $derived({
-		common: 'w-full bg-grey py-3.5 px-6 text-[15px] text-black-800 font-geist font-normal placeholder:text-black-600 rounded-4xl outline-0 border border-transparent invalid:border-red invalid:text-red focus:invalid:text-black-800 focus:invalid:border-transparent',
-		type: typeClasses[type]
-	});
-
-	const radioCustomStyles = $derived({
+	const customRadioStyles = $derived({
 		common: "before:h-4.5 before:w-4.5 before:border-brand-burnt-orange before:-left-0.75 before:-bottom-0.25 relative before:absolute before:rounded-full before:border-2 before:bg-white before:content-['']",
 		selected:
 			'after:h-2.5 after:w-2.5 after:bg-brand-burnt-orange after:absolute after:bottom-0.75 after:left-0.25 after:rounded-full'
@@ -42,7 +36,7 @@
 {#if type === 'radio'}
 	<div
 		class={cn(
-			[radioCustomStyles.common, selected === value ? radioCustomStyles.selected : ''].join(
+			[customRadioStyles.common, selected === value ? customRadioStyles.selected : ''].join(
 				' '
 			)
 		)}
@@ -60,7 +54,7 @@
 			bind:this={radioElement}
 			{name}
 			checked={selected === value}
-			class={cn([cbase.common, cbase.type, restProps.class].join(' '))}
+			class={cn([cbase, restProps.class].join(' '))}
 			tabindex="0"
 		/>
 	</div>
@@ -71,7 +65,7 @@
 		{placeholder}
 		bind:this={input}
 		bind:value
-		class={cn([cbase.common, cbase.type, restProps.class].join(' '))}
+		class={cn([cbase, restProps.class].join(' '))}
 		tabindex="0"
 	/>
 {/if}
