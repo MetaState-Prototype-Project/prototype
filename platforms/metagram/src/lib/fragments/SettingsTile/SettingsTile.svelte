95cfbd55bb62a3cd3607eea3b72d26c7757b71fa
Sahil Garg <sahil@auvo.io>
2025-06-03 14:22:31 +0530

    fix: fixed the use of restProps

--- platforms/metagram/src/lib/fragments/SettingsTile/SettingsTile.svelte
+++ platforms/metagram/src/lib/fragments/SettingsTile/SettingsTile.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
 	import { ArrowRight01Icon } from '@hugeicons/core-free-icons';
 	import { HugeiconsIcon } from '@hugeicons/svelte';
 	import type { HTMLButtonAttributes } from 'svelte/elements';
@@ -12,7 +13,11 @@
 	const { title, currentStatus, onclick, ...restProps }: ISettingsTile = $props();
 </script>
 
-<button {...restProps} class="flex w-full cursor-pointer items-center justify-between" {onclick}>
+<button
+	{...restProps}
+	class={cn(['flex w-full cursor-pointer items-center justify-between', restProps.class].join())}
+	{onclick}
+>
 	<div class="flex flex-col items-start gap-1">
 		<span class="font-semibold">{title}</span>
 		<span class="text-black/60">{currentStatus}</span>
