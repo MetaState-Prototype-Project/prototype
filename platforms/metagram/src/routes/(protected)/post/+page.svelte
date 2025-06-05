5856dcb3666676f0d386d996b24ee72600c8764b
Sahil Garg <sahil@auvo.io>
2025-06-05 14:52:14 +0530

    chore: got rid of non-required code.

--- platforms/metagram/src/routes/(protected)/post/+page.svelte
+++ platforms/metagram/src/routes/(protected)/post/+page.svelte
@@ -1,21 +1,3 @@
 <script lang="ts">
 	import { UploadedPostView } from '$lib/fragments';
-	import { onMount } from 'svelte';
-
-	let images: FileList | null = $state(null);
-    let imageInput: HTMLInputElement;
-	let fileInputTrigger: HTMLButtonElement;
-
-	onMount(() => {
-		console.log(images);
-		console.log(imageInput);
-		if (images === null) {
-		}
-	})
-
-
 </script>
-
-<input type="file" accept="image/*" multiple bind:files={images} bind:this={imageInput} class="hidden" />
-
-<button onclick={() => imageInput.click()} bind:this={fileInputTrigger}>click</button>
