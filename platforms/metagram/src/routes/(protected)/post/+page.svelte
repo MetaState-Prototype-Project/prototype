7ac4cff4eb8d4493a18395e82884694f38b541c4
Sahil Garg <sahil@auvo.io>
2025-06-05 14:40:39 +0530

    feat: trying some hack to get file image input.

--- platforms/metagram/src/routes/(protected)/post/+page.svelte
+++ platforms/metagram/src/routes/(protected)/post/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
-	import { Post } from '$lib/fragments';
-	import { selectedPost } from '$lib/store/store.svelte';
+	import { UploadedPostView } from '$lib/fragments';
+	import { onMount } from 'svelte';
 
-	let post = $state(selectedPost.value);
+	let images: FileList | null = $state(null);
+    let imageInput: HTMLInputElement;
+	let fileInputTrigger: HTMLButtonElement;
+
+	onMount(() => {
+		console.log(images);
+		console.log(imageInput);
+		if (images === null) {
+		}
+	})
+
+
 </script>
 
+<input type="file" accept="image/*" multiple bind:files={images} bind:this={imageInput} class="hidden" />
+
+<button onclick={() => imageInput.click()} bind:this={fileInputTrigger}>click</button>
