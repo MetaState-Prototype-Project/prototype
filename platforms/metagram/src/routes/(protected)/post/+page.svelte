74b859cd3f410006b0c75094b83b8bf9829da822
Sahil Garg <sahil@auvo.io>
2025-06-06 16:01:50 +0530

    feat: correct button placement

--- platforms/metagram/src/routes/(protected)/post/+page.svelte
+++ platforms/metagram/src/routes/(protected)/post/+page.svelte
@@ -1,29 +1,26 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { SettingsTile, UploadedPostView } from '$lib/fragments';
-    import { audience, uploadedImages } from '$lib/store/store.svelte';
-    import { Button } from '$lib/ui';
+	import { audience, uploadedImages } from '$lib/store/store.svelte';
+	import { Button } from '$lib/ui';
 </script>
 
-<UploadedPostView
-    images={uploadedImages.value ?? []}
-    width="w-auto"
-    height="h-60"
-    callback={(i: number) => {
-        if (uploadedImages.value)
-        uploadedImages.value = uploadedImages.value.filter((_, index) => index !== i);
-    }}
-/>
+<section class="h-[75svh] relative flex flex-col gap-10 justify-stretch">
+	<UploadedPostView
+		images={uploadedImages.value ?? []}
+		width="w-auto"
+		height="h-60"
+		callback={(i: number) => {
+			if (uploadedImages.value)
+				uploadedImages.value = uploadedImages.value.filter((_, index) => index !== i);
+		}}
+	/>
 
-<SettingsTile
-    title="Who can see your posts?"
-    currentStatus={audience.value}
-    onclick={() => goto("/post/audience")}
-/>
+	<SettingsTile
+		title="Who can see your posts?"
+		currentStatus={audience.value}
+		onclick={() => goto('/post/audience')}
+	/>
 
-<Button
-    variant="secondary"
-    callback={() => alert("TODO: Post created!")}
->
-Post
-</Button>
+	<Button variant="secondary" callback={() => alert('TODO: Post created!')} class="absolute bottom-0">Post</Button>
+</section>
