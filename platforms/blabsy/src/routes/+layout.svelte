e627ae3bcc9ca77f015740f2495879f76511185f
Merul Dhiman <me@merul.org>
2025-06-07 17:22:31 +0530

    chore: blabsy ready for redesign

--- platforms/blabsy/src/routes/+layout.svelte
+++ platforms/blabsy/src/routes/+layout.svelte
@@ -45,12 +45,12 @@
 </script>
 
 {#if showSplashScreen}
-	<main class="grid h-[100dvh] w-full items-center justify-center">
+	<main class="grid h-[100dvh] w-full items-center justify-center bg-[#000000]">
 		<img src="/images/Logo.svg" alt="logo" />
 	</main>
 {:else}
 	<main
-		class="h-[100dvh] overflow-hidden {page.url.pathname.includes('/profile')
+		class="h-[100dvh] overflow-hidden bg-[#000000] {page.url.pathname.includes('/profile')
 			? 'px-0'
 			: 'px-4'}  md:px-0"
 	>
