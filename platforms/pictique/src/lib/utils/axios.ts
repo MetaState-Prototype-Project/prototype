246ed56fbd0001769daaeac2945c1e7f7c505f30
Merul Dhiman <me@merul.org>
2025-06-16 09:24:26 +0530

    chore: fix auth token thingy

--- platforms/pictique/src/lib/utils/axios.ts
+++ platforms/pictique/src/lib/utils/axios.ts
@@ -3,13 +3,17 @@ import { PUBLIC_PICTIQUE_BASE_URL } from '$env/static/public';
 
 const TOKEN_KEY = 'pictique_auth_token';
 
+let headers: Record<string, any> = {
+    'Content-Type': 'application/json'
+};
+if (getAuthToken()) {
+    headers.authorization = `Bearer ${getAuthToken()}`;
+}
+
 // Create axios instance with base configuration
 export const apiClient: AxiosInstance = axios.create({
     baseURL: PUBLIC_PICTIQUE_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}`
-    }
+    headers
 });
 
 // Utility function to store auth token
@@ -18,9 +22,9 @@ export const setAuthToken = (token: string): void => {
     window.location.href = '/home';
 };
 
-export const getAuthToken = () => {
+export function getAuthToken() {
     return localStorage.getItem(TOKEN_KEY);
-};
+}
 
 // Utility function to remove auth token
 export const removeAuthToken = (): void => {
