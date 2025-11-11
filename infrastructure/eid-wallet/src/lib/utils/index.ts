c87887dbcc7a1db71b6549edb8c653f65bb06e81
gourav <gourav@auvo.io>
2025-11-11 14:26:02 +0530

    feat: swipe-gesture

--- infrastructure/eid-wallet/src/lib/utils/index.ts
+++ infrastructure/eid-wallet/src/lib/utils/index.ts
@@ -1,3 +1,4 @@
 export * from "./mergeClasses";
 export * from "./clickOutside";
 export * from "./capitalize";
+export * from "./swipeGesture";
---
+++ infrastructure/eid-wallet/src/lib/utils/swipeGesture.ts
@@ -0,0 +1,27 @@
+// utils/swipedetect.ts
+export const swipedetect = (el: HTMLElement, callback: (dir: string) => void) => {
+	let startX = 0, startY = 0, distX = 0, distY = 0;
+	let threshold = 100; // min distance for swipe
+	let restraint = 100; // max perpendicular distance
+	let allowedTime = 400; // ms
+	let startTime = 0;
+
+	el.addEventListener("touchstart", (e: TouchEvent) => {
+		const touchobj = e.changedTouches[0];
+		startX = touchobj.pageX;
+		startY = touchobj.pageY;
+		startTime = new Date().getTime();
+	}, false);
+
+	el.addEventListener("touchend", (e: TouchEvent) => {
+		const touchobj = e.changedTouches[0];
+		distX = touchobj.pageX - startX;
+		distY = touchobj.pageY - startY;
+		const elapsedTime = new Date().getTime() - startTime;
+
+		if (elapsedTime <= allowedTime && Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+			const dir = distX < 0 ? "right" : "left";
+			callback(dir);
+		}
+	}, false);
+};
