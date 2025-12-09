637ba08368ffc755284db49d0590807624300d60
Merul Dhiman <me@merul.org>
2025-12-10 00:26:35 +0530

    chore: icon name

--- infrastructure/eid-wallet/src/lib/ui/Toast/Toast.svelte
+++ infrastructure/eid-wallet/src/lib/ui/Toast/Toast.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { toastStore } from "./toast";
     import { cn } from "$lib/utils";
-    import { XIcon } from "@hugeicons/core-free-icons";
+    import { Close01Icon } from "@hugeicons/core-free-icons";
     import { HugeiconsIcon } from "@hugeicons/svelte";
 
     let toasts = $state<Array<{ id: string; message: string; type?: "success" | "error" | "info"; duration?: number }>>([]);
@@ -48,7 +48,7 @@
                 aria-label="Close toast"
             >
                 <HugeiconsIcon
-                    icon={XIcon}
+                    icon={Close01Icon}
                     size={20}
                     strokeWidth={2}
                     className="text-current"
