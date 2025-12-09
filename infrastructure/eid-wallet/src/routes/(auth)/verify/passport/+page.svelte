fb5c8e6dee7127f745d42f873da23ca55fff9cc1
Merul Dhiman <me@merul.org>
2025-12-10 00:56:13 +0530

    fix: inset margins

--- infrastructure/eid-wallet/src/routes/(auth)/verify/passport/+page.svelte
+++ infrastructure/eid-wallet/src/routes/(auth)/verify/passport/+page.svelte
@@ -2,7 +2,7 @@
 import Passport from "../steps/passport.svelte";
 </script>
 
-<div class="fixed inset-0 bg-white z-50 overflow-y-auto" style="margin-top: 50px;">
+<div class="fixed inset-0 bg-white z-50 overflow-y-auto" style="padding-top: 50px; padding-left: 24px; padding-right: 24px;">
     <Passport />
 </div>
 
