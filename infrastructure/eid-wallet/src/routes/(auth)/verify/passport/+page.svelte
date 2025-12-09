571daa356a2204b5a0070888b18b99e644a206db
Merul Dhiman <me@merul.org>
2025-12-10 00:54:52 +0530

    chore: margins

--- infrastructure/eid-wallet/src/routes/(auth)/verify/passport/+page.svelte
+++ infrastructure/eid-wallet/src/routes/(auth)/verify/passport/+page.svelte
@@ -2,7 +2,7 @@
 import Passport from "../steps/passport.svelte";
 </script>
 
-<div class="fixed inset-0 bg-white z-50 overflow-auto" style="padding: 50px;">
+<div class="fixed inset-0 bg-white z-50 overflow-y-auto" style="margin-top: 50px;">
     <Passport />
 </div>
 
--- infrastructure/eid-wallet/src/routes/(auth)/verify/selfie/+page.svelte
+++ infrastructure/eid-wallet/src/routes/(auth)/verify/selfie/+page.svelte
@@ -2,7 +2,7 @@
 import Selfie from "../steps/selfie.svelte";
 </script>
 
-<div class="fixed inset-0 bg-white z-50 overflow-auto" style="padding: 50px;">
+<div class="fixed inset-0 bg-white z-50 overflow-y-auto" style="margin-top: 50px;">
     <Selfie />
 </div>
 
