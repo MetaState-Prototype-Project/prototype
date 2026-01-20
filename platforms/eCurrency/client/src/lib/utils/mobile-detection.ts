36a4ca35becc23fbbefd2be63aaccec2dec7fa1e
Julien Connault <julien.connault@gmail.com>
2026-01-20 12:29:43 +0300

    fix: add W3DS.svg for both eCurrency and eReputation & fix eid Wallet link for eCurrency (#690)

--- platforms/eCurrency/client/src/lib/utils/mobile-detection.ts
+++ platforms/eCurrency/client/src/lib/utils/mobile-detection.ts
@@ -9,6 +9,16 @@ export function getDeepLinkUrl(w3dsUrl: string): string {
 }
 
 export function getAppStoreLink(): string {
-  return "https://apps.apple.com/app/eid-wallet";
-}
+		const userAgent =
+			navigator.userAgent || navigator.vendor || (window as { opera?: string }).opera || '';
+		if (/android/i.test(userAgent)) {
+			return 'https://play.google.com/store/apps/details?id=foundation.metastate.eid_wallet';
+		}
+
+		if (/iPad|iPhone|iPod/.test(userAgent) && !('MSStream' in window)) {
+			return 'https://apps.apple.com/in/app/eid-for-w3ds/id6747748667';
+		}
+
+		return 'https://play.google.com/store/apps/details?id=foundation.metastate.eid_wallet';
+	}
 
