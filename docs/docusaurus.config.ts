bfcad7769fd44cb03675956ce00fb9e62faf2749
SoSweetHam <sosweetham@gmail.com>
2026-01-31 22:33:25 +0530

    fix: docs edit url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,6 +1,6 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -44,7 +44,7 @@ const config: Config = {
             {
                 docs: {
                     sidebarPath: './sidebars.ts',
-                    editUrl: 'https://github.com/MetaState-Prototype-Project/prototype/tree/main/docs/docs/',
+                    editUrl: 'https://github.com/MetaState-Prototype-Project/prototype/tree/main/docs/',
                 },
             } satisfies Preset.Options,
         ],
