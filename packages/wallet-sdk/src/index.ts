8c147eddae5b68bfab37453738d5ad60e5765da9
Merul Dhiman <me@merul.org>
2026-02-10 16:07:54 +0530

    feat(wallet-sdk): implement auth flow (parse w3ds auth, sign session, POST to platform)

--- packages/wallet-sdk/src/index.ts
+++ packages/wallet-sdk/src/index.ts
@@ -4,3 +4,10 @@ export {
   type ProvisionOptions,
   type ProvisionResult,
 } from "./provision";
+export {
+  parseAuthUri,
+  authenticateToPlatform,
+  type ParsedAuthUri,
+  type AuthenticateToPlatformOptions,
+  type AuthenticateToPlatformResult,
+} from "./auth";
