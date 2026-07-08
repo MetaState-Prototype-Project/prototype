ef196987f119c8fe3718805429b2536952ae3db7
Sahil Garg <me.sahil.gg@gmail.com>
2026-07-09 00:54:42 +0530

    fix: fixed the inconsistent header issue in pictique. (#1071)
    * fix: fixed the inconsistent header issue in pictique.
    
    * fix: coderabbit suggestions.

--- platforms/pictique/client/src/routes/(protected)/settings/account/+page.svelte
+++ platforms/pictique/client/src/routes/(protected)/settings/account/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		goto('/settings/account/username');
+		goto(resolve('/settings/account/username'), { replaceState: true });
 	});
 </script>
